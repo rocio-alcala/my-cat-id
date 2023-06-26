6949151dc9d0a14cdd9b89d89d54d36c3a2b0fa4
Rocio Alcala <rro.alcala@gmail.com>
2023-06-26 14:56:08 -0300

    refactor: adding id and switch in api

--- pages/api/cats.tsx
+++ pages/api/cats.tsx
@@ -1,32 +1,41 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { json } from "stream/consumers";
+import { uuid } from 'uuidv4';
+
+//para proxima clase errores
 
 export default async function handlerAddCatForm(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    if (req.method === "POST") {
-      const newCat = { ...req.body, id: req.body.name };
+    switch (req.method) {
+      case "POST":
+        const newCat = { ...req.body, id: uuid() };
 
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newCat),
-      };
-      const response = await fetch("http://localhost:3001/cats", options);
+        const options = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newCat),
+        };
+        const response = await fetch("http://localhost:3001/cats", options);
 
-      if (response.ok) {
-        res.status(200).json({ data: req.body });
-      } else {
-        throw new Error("Error posting cat");
-      }
-    } else {
-      const jsonCats = await fetch("http://localhost:3001/cats");
-      const cats = await jsonCats.json();
-      res.status(200).json(cats);
+        if (response.ok) {
+          res.status(200).json({ data: req.body });
+        } else {
+          throw new Error("Error posting cat");
+        }
+        break;
+      case "GET":
+        const jsonCats = await fetch("http://localhost:3001/cats");
+        const cats = await jsonCats.json();
+        if (jsonCats.ok) {
+          res.status(200).json(cats); 
+        } else {
+          throw new Error("Error fetching cats");
+        }
+        break;
     }
   } catch (err) {
     res.status(500).json(err);
