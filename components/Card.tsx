3925817ea98a9832b7d9f6a1cedd4f75bd3860d7
Rocio Alcala <rro.alcala@gmail.com>
2023-07-08 22:01:09 -0300

    chore: adding card styles

--- components/Card.tsx
+++ components/Card.tsx
@@ -39,53 +39,63 @@ function getAge(birth: string) {
 }
 
 function Card({ cat }: CardProps) {
-  
   return (
     <div className={styles.card}>
-      <h1>{cat.name}</h1>
-      <p>Date of birth: {cat.birth || "not specified"} </p>
-      {cat.birth && (
-        <p>
-          {cat.name} has {getAge(cat.birth)} years old
-        </p>
-      )}
-      <p>Sex: {cat.sex}</p>
-      <p>Rabies vaccine: {cat.rabies ? "Yes" : "No"}</p>
-      {cat.rabiesDate ? (
-        <p>
-          {cat.name} next rabies vaccine is due for{" "}
-          {getNextVaccineDate(cat.rabiesDate, "annual")}
-        </p>
-      ) : (
-        <p>{cat.name} should get the rabies vaccine</p>
-      )}
-      <p>Triple feline vaccine: {cat.tripleFeline ? "Yes" : "No"}</p>
-      {cat.tripleFelineDate ? (
-        <p>
-          {cat.name} next triple feline vaccine is due for{" "}
-          {getNextVaccineDate(cat.tripleFelineDate, "annual")}
-        </p>
-      ) : (
-        <p>{cat.name} should get the triple feline vaccine</p>
-      )}
-      <p>VLFe vaccine: {cat.VLFe ? "Yes" : "No"}</p>
-      {cat.VLFeDate ? (
-        <p>
-          {cat.name} next VLFe vaccine is due for{" "}
-          {getNextVaccineDate(cat.VLFeDate, "annual")}
-        </p>
-      ) : (
-        <p>{cat.name} should get the VLFe vaccine</p>
-      )}
-      {cat.dewormedDate ? (
-        <p>
-          {cat.name} next deworm is due for{" "}
-          {getNextVaccineDate(cat.dewormedDate, "monthly")}
-        </p>
-      ) : (
-        <p>{cat.name} should be deworm</p>
-      )}
-      <p>Colors: {cat.color}</p>
+      <h1 className={styles.cardItemTitle}>{cat.name}</h1>
+      <div className={styles.cardItem}>
+        <p>Date of birth: {cat.birth || "not specified"} </p>
+        {cat.birth && (
+          <p>
+            {cat.name} has {getAge(cat.birth)} years old
+          </p>
+        )}
+      </div>
+      <p className={styles.cardItem}>Sex: {cat.sex}</p>
+      <p className={styles.cardItem}>Colors: {cat.color}</p>
+      <div className={styles.cardItem}>
+        <p>Rabies vaccine: {cat.rabies ? "Yes" : "No"}</p>
+        {cat.rabiesDate ? (
+          <p>
+            {cat.name} next rabies vaccine is due for{" "}
+            {getNextVaccineDate(cat.rabiesDate, "annual")}
+          </p>
+        ) : (
+          <p>{cat.name} should get the rabies vaccine</p>
+        )}
+      </div>
+      <div className={styles.cardItem}>
+        <p>Triple feline vaccine: {cat.tripleFeline ? "Yes" : "No"}</p>
+        {cat.tripleFelineDate ? (
+          <p>
+            {cat.name} next triple feline vaccine is due for{" "}
+            {getNextVaccineDate(cat.tripleFelineDate, "annual")}
+          </p>
+        ) : (
+          <p>{cat.name} should get the triple feline vaccine</p>
+        )}
+      </div>
+      <div className={styles.cardItem}>
+        <p>VLFe vaccine: {cat.VLFe ? "Yes" : "No"}</p>
+        {cat.VLFeDate ? (
+          <p>
+            {cat.name} next VLFe vaccine is due for{" "}
+            {getNextVaccineDate(cat.VLFeDate, "annual")}
+          </p>
+        ) : (
+          <p>{cat.name} should get the VLFe vaccine</p>
+        )}{" "}
+      </div>
+      <div className={styles.cardItem}>
+        <p>Dewormed: {cat.dewormed ? "Yes" : "No"}</p>
+        {cat.dewormedDate ? (
+          <p>
+            {cat.name} next deworm is due for{" "}
+            {getNextVaccineDate(cat.dewormedDate, "monthly")}
+          </p>
+        ) : (
+          <p>{cat.name} should be deworm</p>
+        )}
+      </div>
     </div>
   );
 }
